72c44a810e7c94179da9fbc7a0f93888008e06de
NickP5 <nickp1557@gmail.com>
2026-03-19 11:43:31 -0500

    Added Posts to database
    Made db public and tested adding our demo posts to the posts collection

--- app/src/main/java/com/example/theapp/TestPostList.kt
+++ app/src/main/java/com/example/theapp/TestPostList.kt
@@ -5,12 +5,16 @@ object TestPostList {
         val postList = ArrayList<Post>()
         val post1 = Post("I Am Placing Blocks And Shit", "5/5", "Notch")
         postList.add(post1)
+        post1.saveUserToFirestore()
         val post2 = Post("I Am Stacking Blocks And Shit", "5/5", "AlexeyPajitnov")
         postList.add(post2)
+        post2.saveUserToFirestore()
         val post3 = Post("Nothing Like Arcane", "1/5", "hnzi")
         postList.add(post3)
+        post3.saveUserToFirestore()
         val post4 = Post("jump over spike", "3/5", "blingus")
         postList.add(post4)
+        post4.saveUserToFirestore()
 
         return postList
     }
