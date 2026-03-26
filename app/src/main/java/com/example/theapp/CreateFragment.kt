ff279af2ccac33dcf174ee1f7fd6ca351b11de70
hjones318 <hpjones@gustavus.edu>
2026-03-26 12:08:26 -0500

    add to database functionality complete
    clicking post button puts post into the database

--- app/src/main/java/com/example/theapp/CreateFragment.kt
+++ app/src/main/java/com/example/theapp/CreateFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.theapp.databinding.FragmentCreateBinding
 
 class CreateFragment : Fragment() {
     private var _binding: FragmentCreateBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var clueInput: EditText
+    lateinit var ratingInput: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +27,20 @@ class CreateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        clueInput = view.findViewById(R.id.clueField)
+        ratingInput = view.findViewById(R.id.ratingField)
+
+        binding.postButton.setOnClickListener {
+            val clue = clueInput.text.toString()
+            val rating = ratingInput.text.toString()
+            val name = "hi nick hnzi" // replace with the user's name
+
+            val newPost = Post(clue, rating, name)
+            newPost.saveUserToFirestore()
+
+            Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
