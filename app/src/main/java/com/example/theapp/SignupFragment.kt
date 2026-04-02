44defad072eb7f971e21e70ed98718b4541726a1
hjones318 <131036085+hjones318@users.noreply.github.com>
2026-04-02 00:53:44 -0500

    functional signupfragment
    here you go nick

--- app/src/main/java/com/example/theapp/SignupFragment.kt
+++ app/src/main/java/com/example/theapp/SignupFragment.kt
@@ -1,59 +1,50 @@
 package com.example.theapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import com.example.theapp.databinding.FragmentLoginBinding
+import com.example.theapp.databinding.FragmentSignupBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var user_input: EditText
+    lateinit var email_input: EditText
+    lateinit var pass_input: EditText
+    lateinit var pass_confirm: EditText
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+    ): View {
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        user_input = binding.root.findViewById(R.id.signupUser)
+        email_input = binding.root.findViewById(R.id.signupEmail)
+        pass_input = binding.root.findViewById(R.id.signupPass)
+        pass_confirm = binding.root.findViewById(R.id.passConfirm)
+
+        binding.signupButton.setOnClickListener {
+            val user = user_input.text.toString()
+            val email = user_input.text.toString()
+            val pass = pass_input.text.toString()
+            val confirm = pass_confirm.text.toString()
+
+            //eventually only run this is ue/pass check works
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
