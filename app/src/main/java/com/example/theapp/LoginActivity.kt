63857475b7269ffd5b6ae060bc38ef41b032a3af
hjones318 <131036085+hjones318@users.noreply.github.com>
2026-04-01 21:48:54 -0500

    login button functionality
    just brings you to home screen for now, mostly for nick to work on

--- app/src/main/java/com/example/theapp/LoginActivity.kt
+++ app/src/main/java/com/example/theapp/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.theapp
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.animation.Animation
@@ -15,13 +16,25 @@ import com.example.theapp.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
-    lateinit var ue: EditText
-    lateinit var pass: EditText
+    lateinit var ue_input: EditText
+    lateinit var pass_input: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ue_input = binding.root.findViewById<EditText>(R.id.loginUE)
+        pass_input = binding.root.findViewById<EditText>(R.id.loginPass)
+
+        binding.loginButton.setOnClickListener {
+            val ue = ue_input.text.toString()
+            val pass = pass_input.text.toString()
+
+            //eventually only run this is ue/pass check works
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
         // code to animate background
         val drawable : AnimationDrawable = binding.root.background as AnimationDrawable
@@ -29,7 +42,5 @@ class LoginActivity : AppCompatActivity() {
         drawable.setEnterFadeDuration(1250)
         drawable.setExitFadeDuration(2500)
         drawable.start()
-
-
     }
 }
