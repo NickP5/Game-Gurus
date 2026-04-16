70e6624388a4477ded9e08a019d0634213118c61
hjones318 <hpjones@gustavus.edu>
2026-04-16 12:19:32 -0500

    small UI tweaks
    made stuff a bit easier to interact with

--- app/src/main/java/com/example/theapp/CreateFragment.kt
+++ app/src/main/java/com/example/theapp/CreateFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.RatingBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.theapp.databinding.FragmentCreateBinding
@@ -15,7 +16,7 @@ class CreateFragment : Fragment() {
 
     lateinit var gameInput: EditText
     lateinit var clueInput: EditText
-    lateinit var ratingInput: EditText
+    lateinit var ratingBar: RatingBar
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,18 +32,16 @@ class CreateFragment : Fragment() {
 
         gameInput = view.findViewById(R.id.gameField)
         clueInput = view.findViewById(R.id.clueField)
-        ratingInput = view.findViewById(R.id.ratingField)
+        ratingBar = view.findViewById(R.id.ratingBar)
 
         binding.postButton.setOnClickListener {
             val game = gameInput.text.toString() // will discuss use case in class
             val clue = clueInput.text.toString()
-            val rating = ratingInput.text.toString()
+            val stars = ratingBar.rating.toString()
             val name = "hi nick hnzi" // replace with the user's name
 
-            val newPost = Post(clue, rating, name)
+            val newPost = Post(clue, stars, name)
             newPost.saveUserToFirestore()
-
-            Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
         }
     }
 
