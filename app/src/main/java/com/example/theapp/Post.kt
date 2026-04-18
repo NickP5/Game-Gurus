32cb2ac4f265f2dc815a609e2360553619a2bff2
NickP5 <nickp1557@gmail.com>
2026-04-18 18:31:16 -0500

    Changed Post.kt logic
    Home fragment now takes from database for posts

--- app/src/main/java/com/example/theapp/Post.kt
+++ app/src/main/java/com/example/theapp/Post.kt
@@ -8,6 +8,7 @@ import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.Filter
+import com.google.firebase.firestore.PropertyName
 
 public const val TAG = "Post"
 
@@ -21,10 +22,16 @@ public var uID = 0
 class Post {
     // will likely need to add a postId and comments here,
     // this implementation is mainly for testing recyclerView
+    @get:PropertyName("clue") @set:PropertyName("clue")
     var postClue: String? = null
+    @get:PropertyName("rating") @set:PropertyName("rating")
     var postRating: String? = null // will need to discuss rating system a bit further
+    @get:PropertyName("name") @set:PropertyName("name")
     var postOP: String? = null
+    @get:PropertyName("answer") @set:PropertyName("answer")
     var postAnswer: String? = null
+
+    constructor()
 
 
     constructor(postClue: String?, postRating: String?, postOP: String?, postAnswer: String?) {
@@ -57,64 +64,90 @@ class Post {
         }
     }
 
-    fun savePostToFirestore() {
+    fun getUserID(name: String, callback: (Int) -> Unit){
         val db = Firebase.firestore
-        val documentID = pID
-        val documentID2 = uID
-
-        val post = hashMapOf(
-            "name" to postOP,
-            "rating" to postRating,
-            "clue" to postClue,
-            "answer" to postAnswer,
-            "pID" to documentID
-
-        )
-
-        val postsRef = db.collection("posts")
-        val usersRef = db.collection("users")
-
-        postsRef
-            .whereEqualTo("pID", pID)
-            .whereEqualTo("clue", postClue)
-            .whereEqualTo("rating", postRating).get()
-//            .where(
-//                Filter.or(
-//                    Filter.equalTo("pID", documentID),
-//                    Filter.equalTo("clue", postClue)
-//                )
-            //)
+        db.collection("users")
+            .whereEqualTo("username", name)
+            .get()
             .addOnSuccessListener { querySnapshot ->
-                if (querySnapshot.isEmpty) {
-                    getDbCount(db.collection("posts")) { count ->
-
-                    db.collection("posts")
-                        .document("$count")
-                        .set(post)
-                        .addOnSuccessListener { documentReference ->
-                            Log.d(TAG6, "Post added to Database")
-
-                            usersRef.document("$documentID2")
-                                .update("posts", FieldValue.arrayUnion(documentID))
-                                .addOnSuccessListener {
-                                    Log.d(TAG6, "Post $documentID added to user $documentID2 list")
-                                }
-                        }
+                if (!querySnapshot.isEmpty) {
+                    // Get the first document found with that username
+                    val document = querySnapshot.documents[0]
+                    // Firestore numbers are Long, so we cast to Long then convert to Int
+                    val userID = (document.get("userID") as? Long)?.toInt() ?: -1
+                    callback(userID)
                 } else {
-                    Log.d(TAG6, "Post already exists with pID, $pID")
+                    Log.d(TAG5, "No user found with username: $name")
+                    callback(-1) // Return -1 if user doesn't exist
                 }
             }
+            .addOnFailureListener { e ->
+                Log.e(TAG5, "Error finding userID", e)
+                callback(-1)
+            }
+    }
+
+    fun savePostToFirestore() {
+        //should be used only by the create post button
+        val db = Firebase.firestore
+
+        getDbCount(db.collection("posts")) { count ->
+
+            val documentID = count.toInt()
+            pID = documentID
+            val documentID2 = uID
+
+            val post = hashMapOf(
+                "name" to postOP,
+                "rating" to postRating,
+                "clue" to postClue,
+                "answer" to postAnswer,
+                "pID" to documentID
+
+            )
+
+            val postsRef = db.collection("posts")
+            val usersRef = db.collection("users")
+
+            postsRef
+                //checking if post exists by using and statement against pID, postClue, and postRating
+                .whereEqualTo("pID", pID)
+                .whereEqualTo("clue", postClue)
+                .whereEqualTo("rating", postRating).get()
+                .addOnSuccessListener { querySnapshot ->
+                    if (querySnapshot.isEmpty) {
+                        db.collection("posts")
+                            .document("$documentID")
+                            .set(post)
+                            .addOnSuccessListener { documentReference ->
+                                Log.d(TAG6, "Post added to Database")
+
+                                usersRef.document("$documentID2")
+                                    .update("posts", FieldValue.arrayUnion(documentID))
+                                    .addOnSuccessListener {
+                                        Log.d(
+                                            TAG6, "Post $documentID added to user $documentID2 list"
+                                        )
+                                    }
+                            }
+                    } else {
+                        Log.d(TAG6, "Post already exists with pID, $documentID")
+                    }
+                }
+        }
     }
 
     fun saveUserToFirestore() {
         val db = Firebase.firestore
+
         val documentID = pID
         val documentID2 = uID
         val randomNumber = (1..100).random()
         val postsRef = db.collection("posts")
         val usersRef = db.collection("users")
 
-        val emails = listOf("notch@minecraft.net", "ap@mail.com", "nick@mail.com", "blingus@mail.com")
+        val emails =
+            listOf("notch@minecraft.net", "ap@mail.com", "nick@mail.com", "blingus@mail.com")
         val passwords = listOf("1234", "4321", "nickHnzi", "abcd")
 
         val post = hashMapOf(
@@ -138,10 +171,14 @@ class Post {
             //next step will be posts (list of pID's), friends, recent history / stats
         )
 
-        //Currently going to home fragment is adding new users because we have
-        //a uID variable that doesnt reset to 0
+
+        //Currently we are attempting to add both the four test posts along with their
+        //respective users, I am going to readd them once more then change the logic to
+        //simply take the database information about posts to display them
 
         //initializing users database using four users
+        //getDbCount(db.collection("users")) { userDbCount ->
+        //documentID2 = userDbCount.toInt()
         usersRef.document("$documentID2")
             .get()
             //If user doesn't exist, create document
@@ -157,52 +194,71 @@ class Post {
                         }
                 } else {
                     Log.d(TAG5, "Document: $documentID2 already exists")
-                }
-            }
-
-        val countQuery = postsRef.count()
-
-
-        //if this v is document exists, fuck off and tell them no
-        //else add document to post collection
-        postsRef
-            //.whereEqualTo("pID", pID).whereEqualTo("name", postOP).get()
-            .where(
-                Filter.or(
-                Filter.equalTo("pID", documentID),
-                Filter.equalTo("name", postOP)
-                )
-            ).get()
-            .addOnSuccessListener { querySnapshot->
-                if (querySnapshot.isEmpty()) {
-                    db.collection("posts")
-                        .document("$documentID")
-                        .set(post)
-                        .addOnSuccessListener { documentReference ->
-                            Log.d(TAG, "Post added to Database")
-
-                            pID++
-                            usersRef.document("$documentID2")
-                                .update("posts", FieldValue.arrayUnion(documentID))
-                                .addOnSuccessListener {
-                                    Log.d(TAG, "Post $documentID added to user $documentID2 list")
-                                }
-                        }
-                } else {
-                    Log.d(TAG, "Post already exists, pID = $pID")
-                    db.collection("posts")
-                        .document("$documentID").get()
-                        .addOnSuccessListener { documentReference ->
-
-                            usersRef.document("$documentID2")
-                                .update("posts", FieldValue.arrayUnion(documentID))
-                            Log.d(TAG, "Update user documents with postIDs")
-                        }
+                    uID++
                 }
             }
             .addOnFailureListener { e ->
+                Log.w(TAG5, "Error adding document", e)
+            }
+        //}
+
+
+        //if this v is document exists, fuck off and tell them no
+        //else add document to post collection and then add the post to the
+        //relevant user's post list
+
+        //getDbCount(db.collection("posts")) { postDbCount ->
+            //documentID = postDbCount.toInt()
+            //pID = documentID
+            Log.d(TAG, "pID is $pID")
+            postsRef
+                //.whereEqualTo("pID", pID).whereEqualTo("name", postOP).get()
+                .where(
+                    Filter.or(
+                        Filter.equalTo("pID", documentID),
+                        Filter.equalTo("name", postOP)
+                    )
+                ).get()
+                .addOnSuccessListener { querySnapshot ->
+                    if (querySnapshot.isEmpty) {
+                        db.collection("posts")
+                            .document("$documentID")
+                            .set(post)
+                            .addOnSuccessListener { documentReference ->
+                                Log.d(TAG, "Post added to Database")
+
+                                //getDbCount(db.collection("users")) { userDbCount ->
+                                //documentID2 = userDbCount.toInt()
+                                //uID = documentID2
+                                usersRef.document("$documentID2")
+                                    .update("posts", FieldValue.arrayUnion(documentID))
+                                    .addOnSuccessListener {
+                                        Log.d(
+                                            TAG,
+                                            "Post $documentID added to user $documentID2 list"
+                                        )
+                                    }
+                            }
+                        //}
+                    } else {
+                        Log.d(TAG, "Post already exists, pID = $pID")
+                        db.collection("posts")
+                            .document("$documentID").get()
+                            .addOnSuccessListener { documentReference ->
+
+                                //getDbCount(db.collection("users")) { userDbCount ->
+                                //documentID2 = userDbCount.toInt()
+                                usersRef.document("$documentID2")
+                                    .update("posts", FieldValue.arrayUnion(documentID))
+                                Log.d(TAG, "Update user documents with postIDs")
+                            }
+                    }
+                }
+                //}
+                .addOnFailureListener { e ->
                     Log.w(TAG, "Error adding document", e)
                 }
+        pID ++
         }
-
     }
+//}
