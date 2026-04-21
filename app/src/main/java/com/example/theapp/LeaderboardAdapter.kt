f9f4ad2d0af84c39ad7f01028e8f17b0996e1f1b
Djohn259 <jdaemyen@gmail.com>
2026-04-21 12:10:38 -0500

    Changed fragment data management

--- app/src/main/java/com/example/theapp/LeaderboardAdapter.kt
+++ app/src/main/java/com/example/theapp/LeaderboardAdapter.kt
@@ -7,15 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
 import androidx.core.content.ContextCompat
 import com.example.theapp.databinding.LeaderboardElementBinding
 
 private const val TAG4 = "Leaderboard"
 class LeaderboardAdapter(
-    data: List<List<Any>>,
-    context: Context,
-    private val userName: String?
-) : ArrayAdapter<List<Any>>(context, 0, data) {
+    private var data: List<LeaderboardEntry>?,
+    private val context: Context
+) : BaseAdapter() {
+
+    override fun getCount() = data?.size ?: 0
+    override fun getItem(position: Int) = data?.get(position)
+    override fun getItemId(position: Int) = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val binding: LeaderboardElementBinding
@@ -30,14 +34,19 @@ class LeaderboardAdapter(
             binding = convertView.tag as LeaderboardElementBinding
         }
 
-        val (firstLetter, rank, name, score) = getItem(position)!!
+        val item = data?.get(position)
+        val firstLetter = item?.firstLetter
+        val rank = item?.rank
+        val name = item?.username
+        val score = item?.points
+        val id = item?.userID
         val highlightBackgroundColor = ContextCompat.getDrawable(context, R.drawable.selected_roundedcorners)
         val backgroundColor = ContextCompat.getDrawable(context, R.drawable.roundedcorners)
         val textColor = ContextCompat.getColor(context, R.color.primary)
 
-        Log.d(TAG4, "userName is: $userName , name is: $name")
+//        Log.d(TAG4, "userName is: $userName , name is: $name")
 
-        if (userName == name) {
+        if (loggedInUser == id) {
             binding.leaderboardListItem.background = highlightBackgroundColor
             binding.rankText.setTextColor(Color.BLACK)
             binding.nameText.setTextColor(Color.BLACK)
@@ -54,5 +63,10 @@ class LeaderboardAdapter(
         binding.pointsText.text = score.toString()
 
         return binding.root
+    }
+
+    fun updateData(newData: List<LeaderboardEntry>?) {
+        data = newData
+        notifyDataSetChanged()
     }
 }
